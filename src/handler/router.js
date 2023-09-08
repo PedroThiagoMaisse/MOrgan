9d39b1c90b8875b2e86b83d23f6e0f51633ed5ac
pedro thiago <pedrothiagojosedasilva@gmail.com>
2023-09-07 21:17:24 -0300

    task: making the navbar pretty

--- src/handler/router.js
+++ src/handler/router.js
@@ -1,10 +1,14 @@
 import { writable } from 'svelte/store';
 import planner from '../lib/planner/Main.svelte'
-import projects from '../lib/projects/Main.svelte'
+// import projects from '../lib/projects/Main.svelte'
 import quicknotes from '../lib/quicknotes/Main.svelte'
 import config from '../lib/config/Main.svelte'
-import tester from '../lib/tester/Main.svelte'
+// import tester from '../lib/tester/Main.svelte'
+import calendarIcon from '../assets/icons/calendar.svg'
+import notesIcon from '../assets/icons/notes.svg'
+import settingsIcon from '../assets/icons/settings.svg'
 
+console.log(notesIcon)
 const selected = writable([0, 0])
 const routes = writable([])
 
@@ -14,15 +18,15 @@ routes.set( [
         name: 'main',
         list: [
             // {name: 'Tester', component: tester},
-            {name: 'QuickNotes', component: quicknotes},
-            { name: 'Planner', component: planner },
-            { name: 'Projects', component: projects }
+            {name: 'QuickNotes', component: quicknotes, icon: calendarIcon},
+            { name: 'Planner', component: planner, icon: notesIcon },
+            // { name: 'Projects', component: projects }
         ]
     },
     {
         name: 'config',
         list: [
-            {name: 'Configurações', element: {},component: config}
+            {name: 'Configurações', component: config, icon: settingsIcon}
         ]
     }
 ])
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,30 +5,58 @@
 <main>
     <h1> MOrgan </h1>
 
+    <div class="option">
     {#each routes as element, i (element)}
         <div class="section">
             {#each element.list as option, x (option)}
                 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->   <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <div class="tab {i=== selected[0] && x == selected[1] ? 'yes' : ''}" on:click={() => changeRoute(i, x)} >
-                    <p class='clickable' bind:this={option.element}> {option.name}
-                    </p> 
-                </div>
+                <button class="tab null {i=== selected[0] && x == selected[1] ? 'yes' : ''}" on:click={() => changeRoute(i, x)} >
+                    <img width="32px" height="32px" id="Outlined" src={option.icon}/>
+                </button>
             {/each}
         </div>
     {/each}
+    </div>
 </main>
 
 
 <style>
+    .tab:hover{
+        cursor: pointer;
+        filter: invert(100%) sepia(0%) saturate(5700%) hue-rotate(297deg) brightness(114%) contrast(73%);
+    }
+    .option{
+        padding: 16px 0px 0px 0px;
+        height: calc(100% - 16px);
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+    }
+    .tab img{
+        margin-left: 12px;
+        filter: invert(24%) sepia(26%) saturate(0%) hue-rotate(262deg) brightness(93%) contrast(88%);
+    }
+
+    .yes img{
+        filter: invert(100%) sepia(0%) saturate(5700%) hue-rotate(297deg) brightness(114%) contrast(73%) !important;
+    }
+
     .tab{
-        height: 40px;
-        padding: 20px 0px 0px 12px;
+        display: flex;
+        margin-bottom: 16px;
+        background-color: transparent;
+        border-radius: 0px;
+        border-width: 0px;
+        width: calc(100% - 0px);
+        height: 50px;
+        padding: 8px 0px 8px 6px;
     }
     .yes{
+        padding: 8px 0px 8px 4px !important;
         border-style: solid;
-        border-width: 0px 0px 0px 2px;
-        color: #DDD;
-        border-color: #DDD;
+        border-width: 0px 0px 0px 2px !important;
+        color: #DDD !important;
+        border-color: #DDD !important;
     }
     .section{
         position: relative; 
@@ -36,23 +64,13 @@
         margin-bottom: 16px;
         margin-top: 18px;
     }
-    .clickable{
-        line-height: 100%;
-        font-size: 14px;
-        margin: 0px 0px 8px 0px;
-    }
 
     main h1{
         color: #DDD;
-        font-size: 22px;
+        font-size: 12px;
         font-weight: bold;
         padding: 10px 0px 0px 12px;
         margin: 0px;
-    }
-
-    .tab:hover{
-        text-decoration: underline;
-        cursor: pointer;
     }
 
     main{
@@ -60,7 +78,7 @@
         background-color: #111;
         height: calc(100vh - 10px);
         padding-top: 10px;
-        width: 136px;
+        width: 76px;
         display: block;
         text-align: left;
     }
